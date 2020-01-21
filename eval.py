3de2e2b703605c2e1a59ed78cb632818ecc53516
Игорь Гриценко <igorgricenko@iMac-Igor.local>
2020-01-21 08:16:36 +0300

    eval and utils updated

--- eval.py
+++ eval.py
@@ -1,23 +1,32 @@
 import torch
 from models import NormalisedVGG, Decoder
-from utils import forward_transform
+from utils import load_image, preprocess, deprocess, extract_images
+from ops import style_decorator, wct
 import argparse
 import os
 
 
 parser = argparse.ArgumentParser(description='WCT')
-parser.add_argument('--content-path', type=str, required=True)
-parser.add_argument('--style-path', type=str, required=True)
-parser.add_argument('--save-path', type=str, default='./result.jpg')
+
+parser.add_argument('--content-path', type=str, help='path to the content image')
+parser.add_argument('--style-path', type=str, help='path to the style image')
+parser.add_argument('--content-dir', type=str, help='path to the content image folder')
+parser.add_argument('--style-dir', type=str, help='path to the style image folder')
+
+parser.add_argument('--save-name', type=str, default='result')
+parser.add_argument('--save-dir', type=str, default='./results')
+parser.add_argument('--save-ext', type=str, default='jpg', help='The extension name of the output image')
 parser.add_argument('--encoder-path', type=str, default='encoder/vgg_normalised_conv5_1.pth')
 parser.add_argument('--decoders-dir', type=str, default='decoders')
-parser.add_argument('--content-size', type=int, default=768, help='smaller side size of content image')
-parser.add_argument('--style-size', type=int, default=768, help='smaller side size of style image')
+parser.add_argument('--content-size', type=int, default=768, help='New (minimum) size for the content image')
+parser.add_argument('--style-size', type=int, default=768, help='New (minimum) size for the style image')
+
 parser.add_argument('--style-decorator', type=int, default=1)
-parser.add_argument('--kernel-size', type=int, default=7)
+parser.add_argument('--kernel-size', type=int, default=12)
 parser.add_argument('--stride', type=int, default=1)
 parser.add_argument('--alpha', type=float, default=0.8)
 parser.add_argument('--ss-alpha', type=float, default=0.6)
+
 parser.add_argument('--use-gpu', type=int, default=1)
 parser.add_argument('--gpu', type=str, default=0)
 
@@ -32,7 +41,79 @@ d3 = Decoder('relu3_1', pretrained_path=os.path.join(args.decoders_dir, 'd3.pth'
 d2 = Decoder('relu2_1', pretrained_path=os.path.join(args.decoders_dir, 'd2.pth')).to(device)
 d1 = Decoder('relu1_1', pretrained_path=os.path.join(args.decoders_dir, 'd1.pth')).to(device)
 
+
+def style_transfer(content, style):
+
+    if args.style_decorator:
+        relu5_1_cf = encoder(content, 'relu5_1')
+        relu5_1_sf = encoder(style, 'relu5_1')
+        relu5_1_scf = style_decorator(relu5_1_cf, relu5_1_sf, args.kernel_size, args.stride, args.ss_alpha)
+        relu5_1_recons = d5(relu5_1_scf)
+    else:
+        relu5_1_cf = encoder(content, 'relu5_1')
+        relu5_1_sf = encoder(style, 'relu5_1')
+        relu5_1_scf = wct(relu5_1_cf, relu5_1_sf, args.alpha)
+        relu5_1_recons = d5(relu5_1_scf)
+
+    relu4_1_cf = encoder(relu5_1_recons, 'relu4_1')
+    relu4_1_sf = encoder(style, 'relu4_1')
+    relu4_1_scf = wct(relu4_1_cf, relu4_1_sf, args.alpha)
+    relu4_1_recons = d4(relu4_1_scf)
+
+    relu3_1_cf = encoder(relu4_1_recons, 'relu3_1')
+    relu3_1_sf = encoder(style, 'relu3_1')
+    relu3_1_scf = wct(relu3_1_cf, relu3_1_sf, args.alpha)
+    relu3_1_recons = d3(relu3_1_scf)
+
+    relu2_1_cf = encoder(relu3_1_recons, 'relu2_1')
+    relu2_1_sf = encoder(style, 'relu2_1')
+    relu2_1_scf = wct(relu2_1_cf, relu2_1_sf, args.alpha)
+    relu2_1_recons = d2(relu2_1_scf)
+
+    relu1_1_cf = encoder(relu2_1_recons, 'relu1_1')
+    relu1_1_sf = encoder(style, 'relu1_1')
+    relu1_1_scf = wct(relu1_1_cf, relu1_1_sf, args.alpha)
+    relu1_1_recons = d1(relu1_1_scf)
+
+    return relu1_1_recons
+
+
+if not os.path.exists(args.save_dir):
+    print('Creating save folder at', args.save_dir)
+    os.mkdir(args.save_dir)
+
 with torch.no_grad():
-    forward_transform(device, args.save_path, args.content_path, args.style_path, encoder, [d5, d4, d3, d2, d1],
-                      args.content_size, args.style_size, args.alpha, args.style_decorator,
-                      args.kernel_size, args.stride, args.ss_alpha)
+
+    if args.content_dir and args.style_dir:
+        content_paths = extract_images(args.content_dir)
+        style_paths = extract_images(args.style_dir)
+
+        for i in range(len(content_paths)):
+            content = load_image(content_paths[i])
+            content = preprocess(content, args.content_size)
+            content = content.to(device)
+
+            for j in range(len(style_paths)):
+                style = load_image(style_paths[j])
+                style = preprocess(style, args.style_size)
+                style = style.to(device)
+
+                output = style_transfer(content, style)
+                output = deprocess(output)
+                save_path = '%s/%s_%s.%s' % (args.save_dir, i, j, args.save_ext)
+                print('Output image saved at:', save_path)
+                output.save(save_path)
+    else:
+        content = load_image(args.content_path)
+        content = preprocess(content, args.content_size)
+        content = content.to(device)
+
+        style = load_image(args.style_path)
+        style = preprocess(style, args.style_size)
+        style = style.to(device)
+
+        output = style_transfer(content, style)
+        output = deprocess(output)
+        save_path = '%s/%s.%s' % (args.save_dir, args.save_name, args.save_ext)
+        print('Output image saved at:', save_path)
+        output.save(save_path)
--- utils.py
+++ utils.py
@@ -27,29 +27,19 @@ def deprocess(tensor):
     return transforms.ToPILImage()(tensor)
 
 
-def forward_transform(
-        device, save_path, content_path, style_path, encoder, decoders, content_size,
-        style_size, alpha, decorator, kernel_size=7, stride=1, ss_alpha=0.6
-):
+def extract_images(path):
+    r_ = []
+    valid_ext = ['.jpg', '.png']
 
-    content = preprocess(load_image(content_path), content_size).to(device)
-    style = preprocess(load_image(style_path), style_size).to(device)
+    items = os.listdir(path)
 
-    d5, d4, d3, d2, d1 = decoders
+    for item in items:
+        item_path = os.path.join(path, item)
 
-    if decorator:
-        relu5_1_recons = relu_x_1_style_decorator_transform(content, style, encoder, d5,
-                                                            'relu5_1', kernel_size, stride, ss_alpha)
-    else:
-        relu5_1_recons = relu_x_1_transform(content, style, encoder, d5, 'relu5_1', alpha)
-    print('relu5_1 done')
-    relu4_1_recons = relu_x_1_transform(relu5_1_recons, style, encoder, d4, 'relu4_1', alpha)
-    print('relu4_1 done')
-    relu3_1_recons = relu_x_1_transform(relu4_1_recons, style, encoder, d3, 'relu3_1', alpha)
-    print('relu3_1 done')
-    relu2_1_recons = relu_x_1_transform(relu3_1_recons, style, encoder, d2, 'relu2_1', alpha)
-    print('relu2_1 done')
-    relu1_1_recons = relu_x_1_transform(relu2_1_recons, style, encoder, d1, 'relu1_1', alpha)
-    print('relu1_1 done')
+        _, ext = os.path.splitext(item_path)
+        if ext not in valid_ext:
+            continue
 
-    deprocess(relu1_1_recons).save(save_path)
+        r_.append(item_path)
+
+    return r_
